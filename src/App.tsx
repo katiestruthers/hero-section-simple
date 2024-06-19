50e702986ea69d1877502e6e1eccc93f3d4989f8
Katie Struthers <99215338+katiestruthers@users.noreply.github.com>
2024-06-19 11:15:40 -0500

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css';
+import { Credits } from './components/Credits';
+import { HeroSection } from './components/HeroSection';
+
+function App() {
+  return (
+    <div className="App">
+      <HeroSection />
+      <Credits />
+    </div>
+  );
+};
+
+export default App;
