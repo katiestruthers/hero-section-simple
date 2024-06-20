4608b23fdfdf947f306c044440095a6dc884bb09
Katie Struthers <99215338+katiestruthers@users.noreply.github.com>
2024-06-20 06:04:18 -0500

    mobile layout

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -5,18 +5,24 @@ import image from '../img/prism.png';
 
 export const HeroSection = () => {
   return (
-    <section className="hero flex">
-      <header>
-        <h1 className="font-semibold text-4xl md:text-5xl xl:text-6xl">Well crafted abstract images</h1>
-        <div className="font-normal text-lg md:text-xl">High quality abstract images for your projects, wallpaper and presentations.</div>
-        <a href="#">
-          <button className="text-neutral-900 text-base font-medium bg-white px-4 py-2.5 rounded shadow white-button" type="button">Learn more</button>
-        </a>
-        <a href="#">
-          <button className="text-white text-base font-medium bg-indigo-700 px-4 py-2.5 rounded shadow blue-button" type="button">See pricing</button>
-        </a>
-      </header>
-      <img src={image} alt="Geometric shapes or prisms"></img>
-    </section>
+    <div className="h-lvh">
+      <section className="hero shadow-lg">
+        <div className="flex flex-col gap-8">
+          <header className="flex flex-col gap-4">
+            <h1 className="font-semibold text-4xl md:text-5xl xl:text-6xl">Well crafted abstract images</h1>
+            <div className="font-normal text-lg md:text-xl">High quality abstract images for your projects, wallpaper and presentations.</div>
+          </header>
+          <div className="flex gap-4">
+            <a href="#">
+              <button className="text-neutral-900 text-base font-medium bg-white px-5 py-3 rounded shadow white-button" type="button">Learn more</button>
+            </a>
+            <a href="#">
+              <button className="text-white text-base font-medium bg-indigo-700 px-5 py-3 rounded shadow blue-button" type="button">See pricing</button>
+            </a>
+          </div>
+        </div>
+        <img src={image} alt="Geometric shapes or prisms"></img>
+      </section>
+    </div>
   );
 };
