e671fd4dac624963a35ba5fe356c962d7a41f97c
Katie Struthers <99215338+katiestruthers@users.noreply.github.com>
2024-06-21 03:21:49 -0500

    refactor to CSS

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -7,12 +7,12 @@ export const HeroSection = () => {
   return (
     <div className="h-lvh">
       <section className="hero shadow-lg">
-        <div className="flex flex-col gap-8">
-          <header className="flex flex-col gap-4">
+        <div className="hero__message">
+          <header>
             <h1 className="font-semibold text-4xl md:text-5xl xl:text-6xl">Well crafted abstract images</h1>
             <div className="font-normal text-lg md:text-xl">High quality abstract images for your projects, wallpaper and presentations.</div>
           </header>
-          <div className="flex gap-4">
+          <div className="hero__buttons">
             <a href="#">
               <button className="text-neutral-900 text-base font-medium bg-white px-5 py-3 rounded shadow white-button" type="button">Learn more</button>
             </a>
